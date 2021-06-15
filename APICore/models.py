d8bfd48b1c5566652f441d77a81ca4da265319cd
Kasper <kasperliu129@gmail.com>
2021-06-15 19:07:55 +0800

    Added dynamic items

--- APICore/models.py
+++ APICore/models.py
@@ -5,17 +5,21 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    code1name = models.CharField(max_length=256)
-    code2name = models.CharField(max_length=256)
-    code3name = models.CharField(max_length=256)
-    code1code = models.CharField(max_length=256)
-    code2code = models.CharField(max_length=256)
-    code3code = models.CharField(max_length=256)
     hash = models.CharField(max_length=45)
     user = models.CharField(max_length=256)
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Item(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="items")
+    code = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f"{self.name} has code: {self.code}"
 
 
 class Image(models.Model):
@@ -24,4 +28,4 @@ class Image(models.Model):
     full = models.CharField(max_length=512)
 
     def __str__(self):
-        return self.full
+        return f"{self.full}"
